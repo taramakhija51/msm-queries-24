924033b0a2741cb3319210f8a67ff63fc23d4de2
taramakhija51 <114837329+taramakhija51@users.noreply.github.com>
2024-11-04 03:03:08 +0000

    youngest works

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,22 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+  def directors
+    render({ :template => "misc_templates/directors"})
+  end
+  def youngest_director
+    @youngest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
+   
+    render({ :template => "misc_templates/youngest_director"})
+  end
+  def eldest_director
+    @eldest = Director.where.not({ :dob => nil }).order({ :dob => :desc }).at(0)
+    render({ :template => "misc_templates/eldest_director"})
+  end
+  def movies
+    render({ :template => "misc_templates/movies"})
+  end
+  def actors
+    render({ :template => "misc_templates/actors"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", { :controller => "misc", :action => "directors" })
+  get("/directors/youngest", { :controller => "misc", :action => "youngest_director" })
+  get("/directors/eldest", { :controller => "misc", :action => "eldest_director" })
+  get("/movies", { :controller => "misc", :action => "movies" })
+  get("/actors", { :controller => "misc", :action => "actors" })
 end
